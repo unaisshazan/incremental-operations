e2899fe8d4146e865c95ee52c5abc12b2c0cfc92
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-06-15 14:53:34 +0500

    add .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import Message from './components/Mess';
+import Button from '@material-ui/core/Button';
 
-function App() {
+
+export default function App() {
+  let [count,setcount]= useState(1);
+  let [isMorning,setMorning]=useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={` box ${isMorning ? 'dayLight' : ''} `}>
+<h1> Day time ={isMorning ? 'Morning': 'night'}</h1>
+    <Message counter={count}/>
+
+ <Button onClick={()=> setcount(++count)}> update count </Button>
+    <Button onClick={()=>setMorning(!isMorning)}> update day </Button>
     </div>
   );
 }
 
-export default App;
+
---
+++ src/components/Mess.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Message (props){
+
+return (
+
+
+    <h1>value counter {props.counter}</h1>
+)
+
+
+
+}
